116f8a437e1914fe805a7b85297c2d95ccc8cf06
Rustem Aisariyev <scorus0808@gmail.com>
2023-02-08 21:49:24 -0500

    updated some code

--- grades/src/main/java/com/aisariyev/grades/controllers/GradeController.java
+++ grades/src/main/java/com/aisariyev/grades/controllers/GradeController.java
@@ -3,7 +3,10 @@ package com.aisariyev.grades.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.HashMap;
+import java.util.Map;
 import com.aisariyev.grades.models.Grade;
 import com.aisariyev.grades.models.Show;
 
@@ -19,12 +22,14 @@ public class GradeController {
     }
 
     @GetMapping("/")
-    public String  getShows(Model model){
-        model.addAttribute("first",new Show("Breaking Bad", "Ozymandias", 10.0));
-        model.addAttribute("second",new Show("Attack on Titan", "Perfect Game", 9.9));
-        model.addAttribute("third",new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9));
-        model.addAttribute("fourth",new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9));
-        return "shows";
+    public ModelAndView getShows() {
+        Map<String, Show> model = new HashMap<String, Show>();
+        model.put("first", new Show("Breaking Bad", "Ozymandias", 10.0));
+        model.put("second", new Show("Attack on Titan", "Hero", 9.9));
+        model.put("third", new Show("Attack on Titan", "Perfect Game", 9.9));
+        model.put("fourth", new Show("Star Wars: The Clone Wars", "Victory and Death", 	9.9));
+        model.put("fifth", new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9));
+        return new ModelAndView("shows", model);
     }
 
 }
